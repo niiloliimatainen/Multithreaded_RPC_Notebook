d9ee5f28c6e57a6860e39831cd0c7ef37aa76128
niiloliimatainen <niilo.liimatainen@student.lut.fi>
2021-03-22 15:48:56 +0200

    Version 1.0

--- client.py
+++ client.py
@@ -10,15 +10,16 @@ import datetime
 import json
 
 def main():
-    #Connecting to the server
-    with xmlrpc.client.ServerProxy("http://127.0.0.1:8000") as proxy:
-        #Proxy instance allows us to use methods from the server
-        print(proxy.system.listMethods())
+    # Connecting to the server
+    try:
+        # Proxy instance allows us to use methods from the server
+        proxy = xmlrpc.client.ServerProxy("http://127.0.0.1:8000")
         print("Welcome to the online notebook!")
         while True:
             print("1) Get all topics")
             print("2) Add a new note")
             print("3) Show topic content")
+            print("4) Find additional information from Wikipedia")
             print("0) Exit")
             choice = int(input("Your choice: "))
             
@@ -28,11 +29,18 @@ def main():
                     print(topic)
                 print()
             elif choice == 2:
-                print(proxy.create_note(new_note()))
+                print(proxy.create_note(new_note(1, None)))
                 print()
             elif choice == 3:
                 topic = input("Give a topic: ")
                 print_content(proxy.find_content(topic))
+            elif choice == 4:
+                search_terms = input("Name the search terms: ")
+                info = proxy.query_wikipedia(search_terms)
+                print(info)
+                answer = input("Do you want to append these links under a topic (y/n)?")
+                if answer == "y":
+                    print(proxy.create_note(new_note(0, info)))
                 print()
             elif choice == 0:
                 print("Thank you!")
@@ -40,33 +48,40 @@ def main():
             else:
                 print("Invalid choice!")
                 print()
+    except:
+        print("Can't connect to the server!")
 
 
-#creating json from the user's note 
-def new_note():
+# Creating json from the user's note. Flag tells if the user is appending topic with Wikipedia query or creating a new note. 
+def new_note(flag, info):
     date = str(datetime.date.today().strftime("%d/%m/%Y"))
     time = str(datetime.datetime.now().strftime("%H:%M:%S"))
     timestamp = f"{date} - {time} "
     topic = input("Give a topic: ")
     note_name = input("Note's name: ")
-    text = input("Text: ")
+    if flag:
+        text = input("Text: ")
+    else:
+        text = info
     json_data = json.dumps({"topic": topic, "note_name": note_name, "text":text, "timestamp": timestamp})
     json_object = json.loads(json_data)
     return json_object
 
-def print_content(json_object):
-    if json_object == 0:
-        print("Topic has no entries yet!")
+# Printing the content from a certain topic
+def print_content(content_list):
+    if content_list == 0:
+        print("Topic has no entries yet!\n")
         return
-    elif json_object == -1:
+    elif content_list == -1:
         print("Error happened, try again later!") 
         return
     else:
-        topic = json_object["topic"]
-        note_name = json_object["note_name"]
-        text = json_object["text"]
-        timestamp = json_object["timestamp"]
-        print(f"{topic} - {note_name}:\n   {text}\n{timestamp}")
+        for json_object in content_list:
+            topic = json_object["topic"]
+            note_name = json_object["note_name"]
+            text = json_object["text"]
+            timestamp = json_object["timestamp"]
+            print(f"{topic} - {note_name}:\n   {text}\n{timestamp}\n")
 
 
 if __name__ == "__main__":
--- server.py
+++ server.py
@@ -5,6 +5,7 @@
 # https://docs.python.org/3/library/socketserver.html#socketserver.ThreadingMixIn
 # https://docs.python.org/3/library/xmlrpc.server.html#simplexmlrpcserver-example
 # https://docs.python.org/3/library/xml.etree.elementtree.html
+# https://requests.readthedocs.io/en/master/
 #########################################
 from xmlrpc.server import SimpleXMLRPCServer
 from xmlrpc.server import SimpleXMLRPCRequestHandler
@@ -12,8 +13,9 @@ import xml.etree.ElementTree as ET
 import json
 import socketserver
 import time
+import requests
 
-
+ 
 # Creating threading version from the SimpleXMLRPCServer,which creates a new thread to handle each request.
 class ThreadedSimpleXMLRPCServer (socketserver.ThreadingMixIn, SimpleXMLRPCServer):
     pass
@@ -25,16 +27,10 @@ class RequestHandler(SimpleXMLRPCRequestHandler):
 
 #Creating the server
 def start_server():
-    with ThreadedSimpleXMLRPCServer(("127.0.0.1", 8000),
-                        requestHandler=RequestHandler) as server:
+    try:
+        server = ThreadedSimpleXMLRPCServer(("127.0.0.1", 8000), requestHandler=RequestHandler)
         class Functions:
-            def test(self):
-                print("Sleeping!")
-                time.sleep(20)
-                print("Wake up!")
-                return "Toimii"
-
-            # Get list of topics in database
+            # Get list of topics in the database
             def get_topics(self):
                 root = get_root()
                 topic_list = []
@@ -45,39 +41,77 @@ def start_server():
                 return topic_list
 
             
-            # Creates a new entry under a topic
+            # Creates a new entry under a topic. Wikipedia query or user made note.
             def create_note(self, json_object):
-                root = get_root()
-                if not root:
-                    return "Error happened, try again later!" 
-
-                   
-                return "Note created!"
+                try:
+                    tree = ET.parse("db.xml")
+                    root = tree.getroot()
+                    for child in root:
+                        # If topic exists, append, else create a new topic
+                        if child.attrib["name"] == json_object["topic"]:
+                            for note in child.findall("note"):
+                                if note.get("name") == json_object["note_name"]:
+                                    print("Hä")
+                                    return "Note's name is already taken!"
+                            print("Mitää")
+                            new_note = ET.SubElement(child, "note")
+                            new_note.set("name", json_object["note_name"])
+                        else:
+                            new_topic = ET.SubElement(root, "topic")
+                            new_topic.set("name", json_object["topic"])
+                            new_note = ET.SubElement(new_topic, "note")
+                            new_note.set("name", json_object["note_name"])
+                        text = ET.SubElement(new_note, "text")
+                        text.text = json_object["text"]
+                        timestamp = ET.SubElement(new_note, "timestamp")
+                        timestamp.text = json_object["timestamp"]
+                        tree.write("db.xml")
+                        return "Note created!"
+                except:
+                    return "Error happened, try again later!"
 
             
             # Get contents based on given topic
             def find_content(self, topic):
                 root = get_root()
+                content_list = []
                 if not root:
                     return -1
                 for child in root:
                     if child.attrib["name"] == topic:
-                        note = child.find("note")
-                        note_name = note.get("name")
-                        text = note.find("text").text
-                        timestamp = note.find("timestamp").text
-                        json_data = json.dumps({"topic": topic, "note_name": note_name, "text":text, "timestamp": timestamp})
-                        json_object = json.loads(json_data)
-                        return json_object
+                        for note in child.findall("note"):
+                            note_name = note.get("name")
+                            text = note.find("text").text
+                            timestamp = note.find("timestamp").text
+                            json_data = json.dumps({"topic": topic, "note_name": note_name, "text":text, "timestamp": timestamp})
+                            json_object = json.loads(json_data)
+                            content_list.append(json_object)
+                        return content_list
                 return 0
 
 
+            # Finding relevant Wikipedia pages from user's search terms and returning their links
+            def query_wikipedia(self, search_terms):
+                try:
+                    URL = "https://en.wikipedia.org/w/api.php"
+                    PARAMS = {
+                        "action": "opensearch",
+                        "search": search_terms,
+                        "limit": "3"
+                    }
+                    r = requests.get(url=URL, params=PARAMS)
+                    data = r.json()
+                    data = f"Page titles and links:\n   {data[1]}\n   {data[3]}"
+                    print(data)
+                    return data
+                except:
+                    return "Error happened, try again later!"
 
-         
-        
+
         server.register_instance(Functions())
-        server.register_introspection_functions()
         server.serve_forever()
+    except:
+        print("Error happened in maintaining the server!")
 
 
 #Helper function to get the root from the XML tree
